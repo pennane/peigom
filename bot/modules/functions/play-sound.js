177892664dd9145aeffa6a9684a3da3c61689601
Arttu Pennanen <arttu@pennanen.org>
2019-04-23 14:59:44 +0300

    Wrapped play sound in try catch

--- bot/modules/functions/play-sound.js
+++ bot/modules/functions/play-sound.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
 module.exports.play = function (filename, msg, connection, client) {
     return new Promise((resolve, reject) => {
-        let dispatcher = connection.playFile(filename);
-        dispatcher.on("end", end => {
-            if (msg.guild.me.voiceChannel) {
-                msg.guild.me.voiceChannel.leave();       
-            }
-        });
+        try {
+            let dispatcher = connection.playFile(filename);
+            dispatcher.on("end", end => {
+                if (msg.guild.me.voiceChannel) {
+                    msg.guild.me.voiceChannel.leave();
+                }
+            });
+        } catch (err) {
+            logger.log(3, err)
+        }
+
         resolve();
     });
 }
