9998bee9478985d96502343b480192524943ad72
Arttu Pennanen <arttu@pennanen.org>
2019-05-08 19:29:48 +0300

    Coded 'auta.js' to suit the new rework

--- bot/modules/commands/auta.js
+++ bot/modules/commands/auta.js
@@ -1,5 +1,10 @@
 const config = require('config');
 const Discord = require('discord.js');
+const fs = require('fs')
+const Command = require('../core/command')
+const logger = require('../utilities/activityLogger')
+let commandDir = __dirname
+console.log(commandDir)
 
 const meta = {
     name: "auta",
@@ -15,12 +20,41 @@ const meta = {
     triggers: ["auta", "help"]
 }
 
+function getCommandInfo(path) {
+    let files = []
+    let triggers = {}
+    let commands = {}
+
+    fs.readdirSync(path).forEach(file => {
+        if (file.endsWith(".js")) {
+            files.push(file);
+        }
+    })
+
+    files.forEach(file => {
+        let command;
+        command = new Command(require(path + "/" + file), file)
+        try {
+            command.triggers.forEach((trigger) => {
+                if (triggers.hasOwnProperty(trigger)) {
+                    throw new Error(`Warning! Command ${command.name} interfering with ${triggers.trigger} with trigger ${trigger}`)
+                } else {
+                    triggers[trigger] = command.name
+                }
+            })
+            commands[command.name] = command;
+        } catch (err) {
+            logger.log(13, { file, err, stack: err.stack })
+        }
+    })
+    return { commands, triggers }
+}
+
+module.exports.meta = meta;
+
 module.exports.run = function (msg, client, args) {
     return new Promise((resolve, reject) => {
-       
-       let prefix = config.discord.prefix;
-       msg.reply("sorry! "+prefix+"auta ei toimi tällä hetkellä.")
-/* 
+        let prefix = config.discord.prefix;
         const embed = new Discord.RichEmbed()
             .setAuthor(`${client.user.username}`, `${client.user.avatarURL}`)
             .setTitle(`${config.app.name}  \`${meta.name}\``)
@@ -28,131 +62,113 @@ module.exports.run = function (msg, client, args) {
             .setDescription(meta.embed.desc)
             .setFooter(meta.embed.footer, "https://arttu.pennanen.org/file/thonk.gif")
             .setTimestamp()
-        let unfilteredcfile = CommandExecutor.files();
-        let cfile = {coms: {}}
-       
-       
-        for (i in unfilteredcfile.coms) {
-            if (!(unfilteredcfile.coms[i].meta.redirect)) {
-              cfile.coms[i] = unfilteredcfile.coms[i]
-            }
-        }
 
-        let admin = {};
         if (!args[1]) {
-            embed.addField(`:mega: Tietoa komennoista:`, `\`${prefix}${meta.name} komennot <näytä kuvaukset: true / false>\``);
-            embed.addField(`:loudspeaker: Tietoa admin komennoista:`, `\`${prefix}${meta.name} admin <näytä kuvaukset: true / false>\``);
+            embed.addField(`:mega: Tietoa komennoista:`, `\`${prefix}${meta.name} komennot\``);
+            embed.addField(`:loudspeaker: Tietoa admin komennoista:`, `\`${prefix}${meta.name} admin\``);
             embed.addField(`:thinking: Tietoa botista:`, `\`${prefix}${meta.name} ${config.app.name} \``);
             embed.addField(`:question: Tietoa tietystä komennosta:`, `\`${prefix}${meta.name} <komennon nimi> \``);
             msg.channel.send(embed)
                 .catch(error => console.log(error));
-        } else {
-
-            if (args[1] === "komennot") {
-
-                embed.setTitle("Komennot:");
-                embed.setDescription("kaikki normaalit komennot")
-                let cmds = "";
-                for (i in cfile.coms) {
-
-                    let obj = cfile.coms[i];
-                    let append;
-                    if (args[2] === "true" || args[2] === "tosi" || args[2] === "1") {
-                        append = `\n${prefix}${obj.meta.name}\n\`${obj.meta.desc}\``;
-                    } else {
-                        append = `\n${prefix}${obj.meta.name}`;
-                    }
-                    if (!obj.meta.admin) {
-                        if ((cmds + append).length > 800) {
-                            embed.addField("----", cmds, 1);
-                            if (i === Object.keys(cfile.coms).length) {
-                                cmds = append;
-                                embed.addField("----", cmds, 1);
-                                cmds = "sukablyat";
-                            }
+        } else if (args[1] === "komennot") {
+            embed.setTitle("Komennot:");
+            embed.setDescription("kaikki normaalit komennot")
+            let cmds = "";
+            for (i in commands) {
+                let obj = commands[i];
+                let append;
+                if (args[2] === "true" || args[2] === "tosi" || args[2] === "1") {
+                    append = `\n${prefix}${obj.name}\n\`${obj.description}\``;
+                } else {
+                    append = `\n${prefix}${obj.name}`;
+                }
+                if (!obj.admin) {
+                    if ((cmds + append).length > 800) {
+                        embed.addField("----", cmds, 1);
+                        if (i === Object.keys(commands).length) {
                             cmds = append;
-                        } else {
-                            cmds += append;
-                        }
-                    }
-                }
-                if (!(cmds === "sukablyat")) {
-                    embed.addField("----", cmds, 1);
-                }
-                msg.channel.send(embed)
-                    .catch(error => console.log(error));
-            } else if (args[1] === "admin") {
-
-                embed.setTitle("Admin komennot:");
-                embed.setDescription("kaikki admin komennot")
-                let cmds = "";
-                for (i in cfile.coms) {
-                    let obj = cfile.coms[i];
-                    let append;
-                    if (args[2] === "true" || args[2] === "tosi" || args[2] === "1") {
-                        append = `\n${prefix}${obj.meta.name}\n\`${obj.meta.desc}\``;
-                    } else {
-                        append = `\n${prefix}${obj.meta.name}`;
-                    }
-                    if (obj.meta.admin) {
-                        if ((cmds + append).length > 800) {
                             embed.addField("----", cmds, 1);
-                            if (i === Object.keys(cfile.coms).length) {
-                                cmds = append;
-                                embed.addField("----", cmds, 1);
-                                cmds = "sukablyat";
-                            }
-                            cmds = append;
-                        } else {
-                            cmds += append;
+                            cmds = "__notfilled";
                         }
+                        cmds = append;
+                    } else {
+                        cmds += append;
                     }
                 }
-                if (!(cmds === "sukablyat")) {
-                    embed.addField("----", cmds, 1);
-                }
-                msg.channel.send(embed)
-                    .catch(error => console.log(error));
-
-            } else if (args[1] === config.app.name) {
-                embed.setThumbnail(client.user.avatarURL)
-                    .setTitle(`${config.app.name}  \`tietoa\``)
-                    .setDescription(`Tietoa botista`)
-                    .addField(`:vertical_traffic_light: Versio:`, `${config.app.version}`)
-                    .addField(`:question: Mikä ihmeen ${config.app.name} ?`, `${config.app.name} on tälläne [node.js](https://nodejs.org/) javascript koodattu ärsyttävä discord botti, joka pistää röttöilijät kuriin.`)
-                    .addField(`:1234: Komentojen määrä:`, Object.keys(cfile.coms).length)
-                    .addField(`:file_cabinet: Liityttyjen serverien määrä:`, client.guilds.size)
-                    .addField(`:pencil: Tekijä:`, `@Susse#9904`);
-                msg.channel.send(embed)
-                    .catch(error => console.log(error));
-            } else if (cfile.coms[args[1]]) {
-                let cmd = cfile.coms[args[1]].info;
-                embed.setDescription(`Tietoa komennosta: \`${prefix}${cmd.name}\``)
-                    .addField(`:pencil: Komento toimii näin:`, `\`${cmd.syntax}\``)
-                    .addField(`:gear: Komennon toiminto:`, `${cmd.desc}`)
-                    .addBlankField();
-                if (cmd.admin) {
-                    embed.addField(`:warning: **Huom**`, `Kyseessä on admin komento.`);
-                }
-                msg.channel.send(embed)
-                    .catch(error => console.log(error));
-            } else {
-                embed.addBlankField()
-                    .setTitle(':eyes: Hupsista')
-                    .setDescription(`Antamaasi  \`${prefix}${meta.name}\` toimintoa \`${args[1]}\` ei ole olemassa.`)
-                    .addField(`:pencil: Kokeile \`${prefix}${meta.name} komennot\``, `(tai pelkästään ${prefix}${meta.name})`)
-                    .addBlankField();
-                msgtosend = `Antamaasi \`${prefix}${meta.name}\` toimintoa \`${args[1]}\` ei ole olemassa.`
-                msg.channel.send(embed)
-                    .catch(error => console.log(error));
             }
+            if (cmds !== "__notfilled") {
+                embed.addField("----", cmds, 1);
+            }
+            msg.channel.send(embed)
+                .catch(error => console.log(error));
+        } else if (args[1] === "admin") {
+            embed.setTitle("Admin komennot:");
+            embed.setDescription("kaikki admin komennot")
+            let cmds = "";
+            for (i in commands) {
+                let obj = commands[i];
+                let append;
+                if (args[2] === "true" || args[2] === "tosi" || args[2] === "1") {
+                    append = `\n${prefix}${obj.name}\n\`${obj.description}\``;
+                } else {
+                    append = `\n${prefix}${obj.name}`;
+                }
+                if (obj.admin) {
+                    if ((cmds + append).length > 800) {
+                        embed.addField("----", cmds, 1);
+                        if (i === Object.keys(commands).length) {
+                            cmds = append;
+                            embed.addField("----", cmds, 1);
+                            cmds = "__notfilled";
+                        }
+                        cmds = append;
+                    } else {
+                        cmds += append;
+                    }
+                }
+            }
+            if (!(cmds === "__notfilled")) {
+                embed.addField("----", cmds, 1);
+            }
+            msg.channel.send(embed)
+                .catch(error => console.log(error));
+        } else if (args[1] === config.app.name) {
+            embed.setThumbnail(client.user.avatarURL)
+                .setTitle(`${config.app.name}  \`tietoa\``)
+                .setDescription(`Tietoa botista`)
+                .addField(`:vertical_traffic_light: Versio:`, `${config.app.version}`)
+                .addField(`:question: Mikä ihmeen ${config.app.name} ?`, `${config.app.name} on tälläne [node.js](https://nodejs.org/) discord botti, joka pistää röttöilijät kuriin.`)
+                .addField(`:1234: Komentojen määrä:`, Object.keys(commands).length)
+                .addField(`:file_cabinet: Liityttyjen serverien määrä:`, client.guilds.size)
+                .addField(`:pencil: Kehittäjä:`, `@Susse#9904`);
+            msg.channel.send(embed)
+                .catch(error => console.log(error));
+        } else if (triggers[args[1]]) {
+            let cmd = commands[triggers[args[1]]];
+            embed.setDescription(`Tietoa komennosta: \`${prefix}${cmd.name}\``)
+                .addField(`:pencil: Komento toimii näin:`, `\`${cmd.syntax}\``)
+                .addField(`:gear: Komennon toiminto:`, `${cmd.description}`)
+                .addField(`:book: Komennon liipaisimet:`, `\`${prefix+[...cmd.triggers].join(", "+prefix)}\``)
+                .addBlankField();
+            if (cmd.admin) {
+                embed.addField(`:warning: **Huom**`, `Kyseessä on admin komento.`);
+            }
+            msg.channel.send(embed)
+                .catch(error => console.log(error));
+        } else {
+            embed.addBlankField()
+                .setTitle(':eyes: Hupsista')
+                .setDescription(`Antamaasi  \`${prefix}${meta.name}\` toimintoa \`${args[1]}\` ei ole olemassa.`)
+                .addField(`:pencil: Kokeile \`${prefix}${meta.name} komennot\``, `(tai pelkästään ${prefix}${meta.name})`)
+                .addBlankField();
+            msgtosend = `Antamaasi \`${prefix}${meta.name}\` toimintoa \`${args[1]}\` ei ole olemassa.`
+            msg.channel.send(embed)
+                .catch(error => console.log(error));
         }
 
-
-*/
         resolve();
     });
 
 }
 
-module.exports.meta = meta;
+let { commands, triggers } = getCommandInfo(commandDir)
