f359bc26ea83143a18a13cf9333f851affe4e679
Arttu Pennanen <arttu@pennanen.org>
2019-05-10 19:00:46 +0300

    Added command "of"

---
+++ bot/modules/commands/of.js
@@ -0,0 +1,36 @@
+const sound = require('../utilities/playSound.js');
+const Discord = require('discord.js');
+
+let embed = new Discord.RichEmbed().setColor(0xF4E542);
+
+const meta = {
+    name: "of",
+    admin: false,
+    syntax: "of",
+    desc: "Soittaa of äänen",
+    triggers: ["of"]
+}
+
+module.exports.run = function (msg, client, args) {
+    return new Promise((resolve, reject) => {
+        let userid = args[1] && msg.authorized ? args[1].replace(/\D/g, '') : null
+        let voiceChannel = userid ? msg.guild.members.get(userid).voiceChannel : msg.member.voiceChannel
+        if (voiceChannel && !(msg.guild.voiceConnection)) {
+            voiceChannel.join()
+                .then(connection => {
+                    sound.play('./assets/sound/of.mp3', msg, connection, client)
+                        .then(resolve());
+                });
+        } else if (!msg.member.voiceChannel) {
+            resolve();
+            embed.setTitle(`Botin kommentti:`)
+                .setDescription(`${msg.member.user.username} mene eka jollekki voicechannelille, kid.`);
+            msg.channel.send(embed)
+                .catch(error => console.info(error));
+        } else {
+            resolve();
+        }
+
+    });
+}
+module.exports.meta = meta;
