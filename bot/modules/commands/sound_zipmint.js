b719c3114a4c3a48657e84b41b6c70bd05950529
Arttu Pennanen <arttu@pennanen.org>
2019-05-31 00:08:50 +0300

    Added ,zipmint

---
+++ bot/modules/commands/sound_zipmint.js
@@ -0,0 +1,36 @@
+const sound = require('../utilities/playSound.js');
+const Discord = require('discord.js');
+
+let embed = new Discord.RichEmbed().setColor(0xF4E542);
+
+const meta = {
+    name: "zipmint",
+    admin: false,
+    syntax: "zipmint",
+    desc: "Soittaa tsipmint äänen",
+    triggers: ["zip", "zipmint", "tsipmint"]
+}
+
+module.exports.run = function (msg, client, args) {
+    return new Promise((resolve, reject) => {
+        let userid = args[1] && msg.authorized ? args[1].replace(/\D/g, '') : null
+        let voiceChannel = userid ? msg.guild.members.get(userid).voiceChannel : msg.member.voiceChannel
+        if (voiceChannel && !(msg.guild.voiceConnection)) {
+            voiceChannel.join()
+                .then(connection => {
+                    sound.play('./assets/sound/tsipmint.mp3', msg, connection, client)
+                        .then(resolve());
+                });
+        } else if (!msg.member.voiceChannel) {
+            resolve();
+            embed.setTitle(`Botin kommentti:`)
+                .setDescription(`${msg.member.user.username} mene eka jollekki voicechannelille, kid.`);
+            msg.channel.send(embed)
+                .catch(error => console.info(error));
+        } else {
+            resolve();
+        }
+
+    });
+}
+module.exports.meta = meta;
