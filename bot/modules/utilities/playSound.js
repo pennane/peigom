ba52038df4b7e90940a96cf29c7f46a7ecd888ec
Arttu Pennanen <arttu@pennanen.org>
2019-09-01 22:57:11 +0300

    Redundant console logging

--- bot/modules/utilities/playSound.js
+++ bot/modules/utilities/playSound.js
@@ -4,16 +4,13 @@ module.exports.play = function (filename, msg, connection, client) {
     return new Promise((resolve, reject) => {
         try {
             let dispatcher = connection.playFile(filename);
-            console.log(filename, dispatcher)
             dispatcher.on("end", reason => {
-                console.log(reason)
                 if (msg.guild.me.voiceChannel) {
                     msg.guild.me.voiceChannel.leave();
                 }
             });
             dispatcher.on('error', e => {
-                // Catch any errors that may arise
-                console.log(e);
+                console.info(e);
               });
               
         } catch (err) {
