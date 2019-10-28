e2cada11d94f1e52cabb1d192a9ba65fc68f2376
Arttu Pennanen <arttu@pennanen.org>
2019-10-28 20:07:27 +0200

    Ghetto channel block

---
+++ bot/modules/commands/admin_disable.js
@@ -0,0 +1,61 @@
+const config = require('config');
+const fs = require('fs');
+const Discord = require('discord.js');
+const syntaxEmbed = require('../utilities/syntaxEmbed')
+
+if (!fs.existsSync('./assets/misc/disabledChannels/channels.json')) { fs.writeFileSync('./assets/misc/disabledChannels/channels.json', '{}') }
+let disabledChannels = JSON.parse(fs.readFileSync('./assets/misc/disabledChannels/channels.json', 'utf8'))
+
+let embed = new Discord.RichEmbed().setColor(0xF4E542);
+
+
+const meta = {
+    name: "bottitoimiitäällä",
+    admin: false,
+    syntax: "bottitoimiitäällä #channel <ei/joo>",
+    desc: "Kosmeettisen virtuaalivaluutan pyörittelyyn",
+    triggers: ["bottitoimiitäällä"],
+    type: ["admin", "utility"]
+}
+
+module.exports.run = function (msg, client, args) {
+    return new Promise((resolve, reject) => {
+        if (!args[1]) {
+            let embed = syntaxEmbed({ meta, args })
+            msg.channel.send(embed).catch(err => console.info(err))
+            return resolve();
+        }
+        let channelId = args[1].match(/\d+/)[0];
+        console.log(channelId)
+
+
+        if (!msg.guild.channels.has(channelId)) {
+            let embed = syntaxEmbed({ meta, args })
+            msg.channel.send(embed).catch(err => console.info(err))
+            return resolve();
+        }
+
+        if (args[2] === "joo") {
+            disabledChannels[channelId] = "listening";
+            msg.channel.send(" Peigom kuuntelee taas " + args[1])
+        } else {
+            disabledChannels[channelId] = "disabled";
+            msg.channel.send(" Peigom ei enää kuuntele " + args[1])
+        }
+
+
+
+        console.log(disabledChannels)
+
+        fs.writeFile('./assets/misc/disabledChannels/channels.json', JSON.stringify(disabledChannels), function (err) {
+            if (err) {
+                return console.info(err);
+            }
+        });
+        resolve();
+    });
+}
+
+module.exports.channelData = () => disabledChannels;
+
+module.exports.meta = meta;
--- bot/modules/core/messageParser.js
+++ bot/modules/core/messageParser.js
@@ -2,6 +2,7 @@ const config = require('config')
 const loader = require('./commandLoader')
 const { check } = require('../utilities/spamProtection')
 const badWords = require('../../assets/misc/badwords/badwords.json').badwords
+const disabledChannels = require('../commands/admin_disable').channelData;
 
 let commandDir = __dirname + '/../commands'
 let prefix = config.get("discord.prefix")
@@ -17,14 +18,33 @@ module.exports.parseMsg = function (msg, client) {
             .catch(err => { logger.log(3, err) })
     }
 
+    let disabled = disabledChannels()
+    console.log(disabled)
+    let channelId = msg.channel.id
+    console.log(channelId)
+
+
+
     if (!hasPrefix) return;
 
     let args = msg.content.trim().substr(prefix.length).split(' ')
+
+    console.log(args)
+
     let trigger = args[0].toLowerCase()
+
+
 
     if (!triggers.hasOwnProperty(trigger)) return;
 
     let command = commands[triggers[trigger]]
+
+    if (disabled.hasOwnProperty(channelId) && disabled[channelId] === "disabled" && command.name !== "bottitoimiitäällä") {
+        msg.channel.send("Botti ei toimi tällä tekstikanavalla.").then((msg) => { msg.delete(10000) }).catch(console.log)
+        return;
+    };
+
+
     if (spamProtection.state) {
         check(msg.member.user, command).then((data) => {
             if (data.alreadyAnswered) {
