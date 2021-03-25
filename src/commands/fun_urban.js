0eec30898578fc7a8d6a12ad87144373e98ae522
Arttu Pennanen <arttu@pennanen.org>
2021-03-25 15:38:40 +0200

    Fixed urban dictionary command

--- src/commands/fun_urban.js
+++ src/commands/fun_urban.js
@@ -1,41 +1,41 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const dictionary = require('urban-dictionary')
 const syntaxEmbed = require('../utilities/syntaxEmbed')
 
 const configuration = {
-    name: "urban",
+    name: 'urban',
     admin: false,
-    syntax: "urban <sana>",
-    desc: "Hakee selitteen sanalle",
-    triggers: ["urban", "dictionary", "define"],
-    type: ["fun"]
+    syntax: 'urban <sana>',
+    desc: 'Hakee selitteen sanalle',
+    triggers: ['urban', 'dictionary', 'define'],
+    type: ['fun']
 }
 
+module.exports.executor = async (msg, client, args) => {
+    if (!args[1]) {
+        msg.channel.send(syntaxEmbed({ configuration }))
+        return
+    }
 
+    let toDefine = args.slice(1).join(' ')
 
-module.exports.executor = function (msg, client, args) {
-    return new Promise((resolve, reject) => {
-        if (!args[1]) {
-            let embed = syntaxEmbed({ configuration })
-            msg.channel.send(embed)
-            return resolve()
-        }
-        let definition = [...args].splice(1).join(" ")
-        let index = 0
-        dictionary.term(definition).then(({ entries }) => {
-            let embed = new Discord.MessageEmbed().setColor(0xF4E542);
-            embed.addField(entries[index].word, `
-            \`\`\`${entries[index].definition.replace(/\[|\]/g, "")}\`\`\` 
-            \`\`\`${entries[index].example.replace(/\[|\]/g, "")}\`\`\`
-            [Link](${entries[index].permalink})`)
-                .setFooter(entries[index].author, "https://arttu.pennanen.org/file/thonk.gif")
-                .setTimestamp()
-            msg.channel.send(embed)
-        }).catch(() => {
-            msg.channel.send(`Ei löytyny selitystä sanomalle ${definition}`)
-        })
-        resolve();
-    });
+    try {
+        let results = await dictionary.define(toDefine)
+        let entry = results[0]
+        let embed = new Discord.MessageEmbed().setColor(0xf4e542)
+        embed.setTitle('Urbanaba Sanababa')
+        embed.addField(
+            toDefine,
+            `${entry.definition.replace(/\[|\]/g, '')}\n*${entry.example.replace(/\[|\]/g, '')}*\n[Link](${
+                entry.permalink
+            })`
+        )
+        embed.setFooter(entry.author, 'https://arttu.pennanen.org/file/thonk.gif')
+        embed.setTimestamp()
+        msg.channel.send(embed)
+    } catch (e) {
+        msg.channel.send(`Ei löytyny selitystä sanomalle ${toDefine}`)
+    }
 }
 
-module.exports.configuration = configuration;
+module.exports.configuration = configuration
