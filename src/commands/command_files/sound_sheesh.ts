8946db9ac952e3bd1203d9ffa24a06bffba13253
Arttu Pennanen <arttu@pennanen.org>
2021-04-07 19:26:52 +0300

    sheesh

--- src/commands/command_files/sound_sheesh.ts
+++ src/commands/command_files/sound_sheesh.ts
@@ -2,15 +2,15 @@ import Command, { CommandConfiguration, CommandExecutor } from '../Command'
 import playSound from '../../sound_handling/playSound'
 
 const configuration: CommandConfiguration = {
-    name: 'sheesh',
+    name: 'bruh',
     admin: false,
-    syntax: 'sheesh',
-    desc: 'sheesh sound effect #2',
-    triggers: ['sheesh', 'sheets', 'cheese'],
+    syntax: 'bruh',
+    desc: 'bruh sound effect #2',
+    triggers: ['bruh', 'bro'],
     type: ['sound']
 }
 
-let soundfile = './assets/sound/sheesh.mp3'
+let soundfile = './assets/sound/bruv.mp3'
 
 const executor: CommandExecutor = async (message, client, args) => {
     playSound({ soundfile, message })
